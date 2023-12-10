9d3ba67e4ed600d1a50cd0fdd88dabcebb29a436
Mike Dice <mikedice@microsoft.com>
2023-12-10 12:07:09 -0800

    Remvoe the template code

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,10 @@ import './App.css'
 import TreeView from './Controls/TreeView/TreeView'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>El Portal Controls</h1>
+      <h2>Tree control</h2>
       <div>
         <TreeView></TreeView>
       </div>
