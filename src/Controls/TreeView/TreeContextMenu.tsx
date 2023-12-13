eb20db73b837ccfa4d08217b81d98a110758c38e
Mike Dice <mikedice@microsoft.com>
2023-12-12 17:21:04 -0800

    updated

--- src/Controls/TreeView/TreeContextMenu.tsx
+++ src/Controls/TreeView/TreeContextMenu.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ContextMenuSelection } from './ContextualListItem';
 import {
     makeStyles,
+    shorthands
   } from "@fluentui/react-components";
 
 interface TreeContextMenuProps {
@@ -13,27 +14,26 @@ interface TreeContextMenuProps {
 const menuStyles = makeStyles({
     menu:{
         position: 'absolute',
-        top:''
+        backgroundColor: 'white',
+        boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
+        ...shorthands.padding('8px')
     }
-
-
 });
+
 export const TreeContextMenu: React.FC<TreeContextMenuProps> = ({ x, y, onSelected }) => {
     const handleMenuItemClick = (event: React.MouseEvent<HTMLDivElement>) => {
         event.preventDefault();
         onSelected({x:event.clientX, y:event.clientY, selectedId: event.currentTarget.id, selectedValue:event.currentTarget.textContent!})
     };
+    const styles = menuStyles()
 
     return (
         <div
             style={{
-                position: 'absolute',
                 top: y,
-                left: x,
-                backgroundColor: 'white',
-                boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
-                padding: '8px',
+                left: x
             }}
+            className={styles.menu}
             onContextMenu={handleMenuItemClick}
         >
             {/* Add your context menu items here */}
